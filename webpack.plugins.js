3cbe05206cadb7e8f5eba825c272bfe836cbeb6b
ariakm25 <ariakm25@gmail.com>
2021-05-20 08:14:49 +0700

    add CopyWebpackPlugin

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,15 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = [
-  new ForkTsCheckerWebpackPlugin()
+  new ForkTsCheckerWebpackPlugin(),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, "assets"),
+        to: path.resolve(__dirname, ".webpack/renderer", "assets"),
+      },
+    ],
+  }),
 ];
